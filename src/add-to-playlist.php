2d7c06b3b084cb33fb63e66eea849e07dd1b977e
Paulo Gobetti <paulogobettig@outlook.com>
2023-06-18 21:30:22 -0300

    feat: add to playlist function

--- src/add-to-playlist.php
+++ src/add-to-playlist.php
@@ -1,6 +1,17 @@
 <?php
 
-    $playlistID = urldecode($_GET['playlist']);
-    $musicID = urldecode($_GET['music']);
+    $data = file_get_contents("../app/data/playlist.json");
+    $json = json_decode($data);
 
-    echo 'Playlist ID: ' . $playlistID . ' e Music ID: ' . $musicID . '<br> ';
+    $playlistID = urldecode($_GET['playlist']);
+    $music = urldecode($_GET['music']);
+
+    foreach($json as $playlist) {
+        if($playlist->playlist_id ==  $playlistID) {
+            array_push($playlist->tracks, $music);
+        }
+    }
+
+    $handle = fopen('../app/data/playlist.json', 'w');
+    fwrite($handle, json_encode($json, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_HEX_TAG | JSON_HEX_QUOT));
+    fclose($handle);
